449d00768f1914ca3c6ed9f883599e12dfff4241
parkma99 <park-ma@hotmail.com>
2021-11-07 23:57:32 +0800

    Milestone 1-4

--- deet/src/debugger.rs
+++ deet/src/debugger.rs
@@ -1,20 +1,30 @@
 use crate::debugger_command::DebuggerCommand;
+use crate::dwarf_data::{DwarfData, Error as DwarfError};
 use crate::inferior::Inferior;
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
-
 pub struct Debugger {
     target: String,
     history_path: String,
     readline: Editor<()>,
     inferior: Option<Inferior>,
+    debug_data: DwarfData,
 }
 
 impl Debugger {
     /// Initializes the debugger.
     pub fn new(target: &str) -> Debugger {
-        // TODO (milestone 3): initialize the DwarfData
-
+        let debug_data = match DwarfData::from_file(target) {
+            Ok(val) => val,
+            Err(DwarfError::ErrorOpeningFile) => {
+                println!("Could not open file {}", target);
+                std::process::exit(1);
+            }
+            Err(DwarfError::DwarfFormatError(err)) => {
+                println!("Could not debugging symbols from {}: {:?}", target, err);
+                std::process::exit(1);
+            }
+        };
         let history_path = format!("{}/.deet_history", std::env::var("HOME").unwrap());
         let mut readline = Editor::<()>::new();
         // Attempt to load history from ~/.deet_history if it exists
@@ -25,6 +35,7 @@ impl Debugger {
             history_path,
             readline,
             inferior: None,
+            debug_data: debug_data,
         }
     }
 
@@ -32,18 +43,92 @@ impl Debugger {
         loop {
             match self.get_next_command() {
                 DebuggerCommand::Run(args) => {
-                    if let Some(inferior) = Inferior::new(&self.target, &args) {
-                        // Create the inferior
-                        self.inferior = Some(inferior);
-                        // TODO (milestone 1): make the inferior run
-                        // You may use self.inferior.as_mut().unwrap() to get a mutable reference
-                        // to the Inferior object
+                    if self.inferior.is_some() {
+                        println!("some process running");
                     } else {
-                        println!("Error starting subprocess");
+                        if let Some(inferior) = Inferior::new(&self.target, &args) {
+                            self.inferior = Some(inferior);
+                            match self
+                                .inferior
+                                .as_mut()
+                                .unwrap()
+                                .continue_run()
+                                .expect("error")
+                            {
+                                crate::inferior::Status::Stopped(_sig, _rip) => {
+                                    println!("Child stopped by signal ({})", _sig,);
+                                    let line = self.debug_data.get_line_from_addr(_rip);
+                                    if line.is_some() {
+                                        println!("Stopped at {}", line.unwrap());
+                                    }
+                                }
+                                crate::inferior::Status::Exited(_code) => {
+                                    println!("Child exited (status {})", _code);
+                                    self.inferior = None;
+                                }
+                                crate::inferior::Status::Signaled(_sig) => {
+                                    println!("Child exited due to signal ({})", _sig);
+                                    self.inferior = None;
+                                }
+                            }
+                        } else {
+                            println!("Error starting subprocess");
+                        }
                     }
                 }
                 DebuggerCommand::Quit => {
-                    return;
+                    if self.inferior.is_none() {
+                        return;
+                    } else {
+                        let pid = self
+                            .inferior
+                            .as_mut()
+                            .unwrap()
+                            .kill()
+                            .expect("error when kill process");
+                        println!("Killing running inferior (pid {})", pid);
+                        return;
+                    }
+                }
+                DebuggerCommand::Continue => {
+                    if self.inferior.is_none() {
+                        println!("nothing is running")
+                    } else {
+                        match self
+                            .inferior
+                            .as_mut()
+                            .unwrap()
+                            .continue_run()
+                            .expect("error")
+                        {
+                            crate::inferior::Status::Stopped(_sig, _rip) => {
+                                println!("Child stopped by signal ({})", _sig);
+                                let line = self.debug_data.get_line_from_addr(_rip);
+                                if line.is_some() {
+                                    println!("Stopped at {}", line.unwrap());
+                                }
+                            }
+                            crate::inferior::Status::Exited(_code) => {
+                                println!("Child exited (status {})", _code);
+                                self.inferior = None;
+                            }
+                            crate::inferior::Status::Signaled(_sig) => {
+                                println!("Child exited due to signal ({})", _sig);
+                                self.inferior = None;
+                            }
+                        }
+                    }
+                }
+                DebuggerCommand::Backtrace => {
+                    if self.inferior.is_none() {
+                        println!("nothing is runing!");
+                    } else {
+                        self.inferior
+                            .as_mut()
+                            .unwrap()
+                            .print_backtrace(&self.debug_data)
+                            .expect("ssss");
+                    }
                 }
             }
         }
--- deet/src/inferior.rs
+++ deet/src/inferior.rs
@@ -3,7 +3,10 @@ use nix::sys::signal;
 use nix::sys::wait::{waitpid, WaitPidFlag, WaitStatus};
 use nix::unistd::Pid;
 use std::process::Child;
+use std::process::Command;
+use std::os::unix::process::CommandExt;
 
+use crate::dwarf_data::DwarfData;
 pub enum Status {
     /// Indicates inferior stopped. Contains the signal that stopped the process, as well as the
     /// current instruction pointer that it is stopped at.
@@ -34,14 +37,65 @@ impl Inferior {
     /// Attempts to start a new inferior process. Returns Some(Inferior) if successful, or None if
     /// an error is encountered.
     pub fn new(target: &str, args: &Vec<String>) -> Option<Inferior> {
-        // TODO: implement me!
-        println!(
-            "Inferior::new not implemented! target={}, args={:?}",
-            target, args
-        );
-        None
+       let mut cmd = Command::new(target);
+       cmd.args(args);
+        unsafe {
+            cmd.pre_exec(child_traceme);
+        }
+        let child = cmd.spawn().expect("error when create child process");
+        let inferior = Inferior {child : child};
+        match inferior.wait(None).ok()?{
+            Status::Stopped(sig, _) => {
+                if sig==signal::Signal::SIGTRAP{
+                    return Some(inferior);
+                }else {
+                    return None;
+                }
+            }
+            Status::Exited(_) => return None,
+            Status::Signaled(_) => return None,
+        } 
     }
-
+    pub fn continue_run(&self) -> Result<Status,nix::Error>{
+        ptrace::cont(self.pid(),None)?;
+        self.wait(None)
+    }
+    pub fn kill(&mut self) ->Result<Pid,std::io::Error>{
+        let pid = self.pid();
+        match self.child.kill(){
+            Ok(_) => {
+                return Ok(pid);
+            }
+            Err(_err) => Err(_err),
+        } 
+    }
+    pub fn print_backtrace(&self,debug_data: &DwarfData) -> Result<(), nix::Error>{
+        let pid = self.pid();
+        let regs = ptrace::getregs(pid).expect("erro when get regs");
+        let mut rip = regs.rip as usize;
+        let mut rbp = regs.rbp as usize;
+        loop {
+            let fun = debug_data.get_function_from_addr(rip);
+            let line = debug_data.get_line_from_addr(rip);
+            match (&fun,&line) {
+                (None, None) => println!("unknown function unknown file"),
+                (None, Some(_line)) =>println!("unknown function ({})",_line),
+                (Some(_fun), None) =>println!("{},unknown file",_fun),
+                (Some(_fun), Some(_line)) => println!("{} ({})",_fun,_line),
+            }
+            match &fun {
+                Some(_fun) => {
+                    if _fun == "main"{
+                        break;
+                    }
+                }
+                None => break,
+            }
+            rip = ptrace::read(self.pid(), (rbp+8) as ptrace::AddressType)? as usize;
+            rbp = ptrace::read(self.pid(), rbp as ptrace::AddressType)? as usize;
+        }
+        Ok(())
+    } 
     /// Returns the pid of this inferior.
     pub fn pid(&self) -> Pid {
         nix::unistd::Pid::from_raw(self.child.id() as i32)
